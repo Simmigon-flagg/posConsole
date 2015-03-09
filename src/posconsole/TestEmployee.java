44bea89eb2b89a373dd671ffec5d2f04a0f35065
Jovy Cortez <jovycortez11@yahoo.com>
2015-03-09 19:03:10 -0400

    TestEmployee tested

--- src/posconsole/TestEmployee.java
+++ src/posconsole/TestEmployee.java
@@ -2,5 +2,11 @@
 package posconsole;
 
 public class TestEmployee {
+    public static void main (String[]args){   
+    Employee employee1 = new Employee("Simmigon", "Flagg", "1234");
+        System.out.println(employee1);
     
+    Employee employee2 = new Employee("Jonah", "Cortez", "2345");
+        System.out.println("Employee Name: " + employee2.getFirstName()+ " "+employee2.getLastName());
+}
 }
