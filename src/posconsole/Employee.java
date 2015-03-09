3fbe3d36bd5fb6f336de4982f38f0069a7b7c556
Simmigon-flagg <simmigon.flagg@gmail.com>
2015-03-09 18:12:42 -0400

    Classes Added
    employee  waiter and table

---
+++ src/posconsole/Employee.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package posconsole;
+
+/**
+ *
+ * @author Flagg
+ */
+public class Employee {
+    String firstName;
+    String LastName;
+    String empId;    
+}
